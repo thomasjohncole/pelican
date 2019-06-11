e85a189e585aade3752c1837194b63a15f65d873
thomasjohncole <colecode@gmail.com>
2019-06-11 23:46:52 +0800

    Update publishconf.py for githubpages URL

--- publishconf.py
+++ publishconf.py
@@ -11,11 +11,14 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://thomasjohncole.com'
+SITEURL = 'https://thomasjohncole.github.io/pelican'
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+# FEED_ALL_ATOM = 'feeds/all.atom.xml'
+# CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
+
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
 
 DELETE_OUTPUT_DIRECTORY = True
 
